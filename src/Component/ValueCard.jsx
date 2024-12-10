6dc62c3d555c3014d172d8e27b00f191f9f03ee2
akintola4 <akintope830@gmail.com>
2024-12-10 07:24:03 +0100

    fixed

--- src/Component/ValueCard.jsx
+++ src/Component/ValueCard.jsx
@@ -6,11 +6,14 @@ function ValueCard({ info }) {
       className="flex flex-col gap-4 h-full border hover:shadow-[rgba(17,_17,_26,_0.1)_0px_0px_16px] p-10 dark:hover:shadow-[rgba(247,240,_252,_0.1)_0px_0px_10px] rounded-lg" 
       data-aos="fade-right"
     >
+      <div className='p-2 md:p-2 flex items-center rounded-full  xl:p-4 bg-btn-base-light w-2/12 md:w-2/12 lg:w-2/12  opacity-50 dark:opacity-100 dark:bg-btn-base-dark  xl:w-2/12'>
       <img 
         src={`img/${info.img}`} 
-        className="p-2 md:p-2 rounded-full  xl:p-4 bg-btn-base-light w-2/12 md:w-2/12 lg:w-2/12  opacity-50 dark:opacity-100 dark:bg-btn-base-dark  xl:w-2/12" 
+        className="w-full" 
         alt={info.title} 
       />
+      </div>
+      
       <h4 className="text-lg text-gray-950 dark:text-white font-bold font-sans">
         {info.title}
       </h4>
